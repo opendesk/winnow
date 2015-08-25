5bbfc50a4955cf5bb67a0ffc98f80112ba3b3c21
James Arthur <thruflo@gmail.com>
2015-08-25 15:44:20 +0100

    release: bump to 0.1.1.

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup, find_packages
 
-setup(name='winnow',
-    version='0.1',
-    description='Product family publishing and manipulation',
+setup(
+    name='winnow',
+    version='0.1.1',
+    description='Winnow is a JSON-schema based library for publishing and manipulating families of products.',
+    url='https://github.com/opendesk/winnow',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7.6',
+        'Programming Language :: Python :: 2.7',
         'Intended Audience :: Manufacturing',
         'License :: Public Domain'
     ],
     author='Paul Harter',
     author_email='username: paul, domain: opendesk.cc',
+    license='UNLICENSE',
     install_requires=['jsonschema'],
     packages=find_packages('src'),
     package_data={'': ['*.json']},
     package_dir={'': 'src'},
     include_package_data=True,
-    zip_safe=False)
-
+    zip_safe=False
+)
